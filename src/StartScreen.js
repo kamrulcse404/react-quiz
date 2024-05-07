e8516923c8aa6caab38049892bf81ade09711b15
kamrulcse404 <kamrul.h01960@gmail.com>
2024-05-07 19:51:02 +0600

    onclick question render

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const StartScreen = ({ numQuestions }) => {
+const StartScreen = ({ numQuestions, dispatch }) => {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz !</h2>
       <h3>{numQuestions} question to testy your mastery</h3>
-      <button className="btn btn-ui">Lets's start</button>
+      <button className="btn btn-ui" onClick={() => dispatch({type: "start"})}>Lets's start</button>
     </div>
   );
 };
